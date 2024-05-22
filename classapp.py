6cc897f54b3bb7a648857abd83f638a52ab1e61b
luanappsilvaa <168466309+luanappsilvaa@users.noreply.github.com>
2024-05-22 14:49:12 -0300

    Update classapp.py

--- classapp.py
+++ classapp.py
@@ -5,3 +5,6 @@ st.title('Salut')
 with st.sidebar:
   st.header('Salut')
   st.write('Seu aplicativo da dicas de saúde')
+  st.caption('Criado por Luana P. Silva')
+
+st.write('Mosso aplicativo tm o foco de oferever dicas sobre corrida, alimentação e saúde')
