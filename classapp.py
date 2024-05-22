9542001f4258bb7a722d6dcd29157bb11ec72491
luanappsilvaa <168466309+luanappsilvaa@users.noreply.github.com>
2024-05-22 15:00:09 -0300

    Update classapp.py

--- classapp.py
+++ classapp.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
-st.title('Salut')
+st.title('Rio Eats')
 
 with st.sidebar:
-  st.header('Salut')
-  st.write('Seu aplicativo da dicas de saúde')
+  st.header('Rio Eats')
+  st.write('Seu aplicativo da dicas para restaurantes')
   st.caption('Criado por Luana P. Silva')
 
-st.write('Mosso aplicativo tem o foco de oferever dicas sobre corrida, nutrição e notícias de saúde')
+st.write('Mosso aplicativo tem o foco de oferecer os restaurantes mais próximos perto de você ')
 tab_corrida, tab_nutri, tab_news = st.tabs(["Corrida de Rua", "Nutrição", "Notícias"])
