de1af5996f62fd549f581913c8a44b3099824367
luanappsilvaa <168466309+luanappsilvaa@users.noreply.github.com>
2024-05-22 15:21:36 -0300

    classapp.py

--- classapp.py
+++ classapp.py
@@ -10,15 +10,4 @@ with st.sidebar:
 st.write('Mosso aplicativo tem o foco de oferecer os restaurantes mais próximos perto de você ')
 tab_corrida, tab_nutri, tab_news = st.tabs(["Corrida de Rua", "Nutrição", "Notícias"])
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Meu Site</title>
-</head>
-<body>
-    <h1>Meu Site</h1>
-    <img src="https://raw.githubusercontent.com/luanappsilvaa/class.py/main/rio%20eats.jpg" alt="Rio Eats">
-</body>
-</html>
+st.image('rio eats.jpg')
